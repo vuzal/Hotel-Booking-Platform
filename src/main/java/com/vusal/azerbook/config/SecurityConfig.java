4b31612045d3e02d502476eb19053c70eccc7f92
Vusal Abbasov <vusalabbasovredmi9@gmail.com>
2026-03-23 23:21:44 +0400

    Vercel link added Cors configuration

--- src/main/java/com/vusal/azerbook/config/SecurityConfig.java
+++ src/main/java/com/vusal/azerbook/config/SecurityConfig.java
@@ -91,7 +91,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000","hotel-booking-platform-sch6.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
